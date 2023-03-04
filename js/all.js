fc61280aa7d9e5243bcb4a926385fcf9f6354f66
tommy726 <zhu86726@gmail.com>
2023-03-04 18:18:52 +0800

    upload

---
+++ js/all.js
@@ -0,0 +1,40 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})();
+
+// activateTab
+function activateTab(tabId) {
+  let tabPane = document.getElementById(tabId);
+  let activeTabButton = document.querySelector('.list-group-item.active');
+
+  activeTabButton.classList.remove('active');
+  document.getElementById(activeTabButton.getAttribute('href').substring(1)).classList.remove('show', 'active');
+
+  let newTabButton = document.querySelector('[aria-controls="' + tabId + '"]');
+  newTabButton.classList.add('active');
+  tabPane.classList.add('show', 'active');
+
+  history.pushState({}, "", '#' + tabId);
+
+  window.addEventListener("load", function(){
+    setTimeout(function(){
+      window.scrollTo(0, 0);
+    }, 0);
+  });
+};
