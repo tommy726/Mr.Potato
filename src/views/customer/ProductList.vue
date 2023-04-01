b4710004096913bd2311176132fe788eb0d8e61d
tommy726 <zhu86726@gmail.com>
2023-04-02 00:18:24 +0800

    Update ProductList.vue

--- src/views/customer/ProductList.vue
+++ src/views/customer/ProductList.vue
@@ -63,6 +63,7 @@ export default {
     ...mapActions('productsModules', ['getProducts']),
     getCategory() {
       this.productCategory = this.$route.params.productCategory;
+      window.scrollTo(0, 0);
     },
   },
   computed: {
